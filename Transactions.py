8f812c1209d7a78171ff0f260f92570200ccca50
pathel <polegend@stud.ntnu.no>
2023-02-04 19:16:10 +0100

    diveded python classes

---
+++ Transactions.py
@@ -0,0 +1,8 @@
+class Transaction:
+    def __init__(self):
+        self.sender = "" #identified by the public key
+        self.receiver = "" #identified by the public key
+        self.amount = 0  #amount of money beign sent after commission is taken by the miner
+        self.date = "" 
+        self.signature = "" 
+        self.date = ""

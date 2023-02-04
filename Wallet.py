02301656112382541a9ee41c56fe987ee6df90fb
pathel <polegend@stud.ntnu.no>
2023-02-04 22:53:34 +0100

    added transaction methods and wallet methods

--- Wallet.py
+++ Wallet.py
@@ -4,3 +4,15 @@ class Wallet:
         self.public_key = "" #the public key is the id of the wallet to be broadcasted
         self.token = "" 
         self.address = "" #hash of the token API 
+        self.amount = 0
+
+    
+    def getPublicKey(self):
+        return self.public_key
+    
+    def getToken(self):
+        return self.token
+    
+    def getAddress(self):
+        return self.address
+    

1e7b660a346213611e258abbfed19bc792526eac
Tomas <tomas.espana@student-cs.fr>
2023-02-05 09:31:58 +0100

    view blockchain

--- view_blockchain.py
+++ view_blockchain.py
@@ -1,53 +1,39 @@
-
+from datetime import date
 import streamlit as st
+from Block import Block
+from Transactions import Transaction
 
 st.title("Blockchain Interface")
 
-blocks = [] #list of blocks
-
-
-for block in blocks:
-    #create a line corresponding to the block
-
-
-
+trans0 = Transaction("sender", "receiver", 10, date.today())
+trans1 = Transaction("sender1", "receiver1", 20, date.today())
+block0 = Block(0, [trans0, trans1], "miner")
+block1 = Block(1, [trans0, trans1], "miner1")
+blocks = [block0, block1] #list of blocks
 
 
 
+#creates st.buttons one above each other
+for i in range(len(blocks)):
 
+    if st.button("Block " + str(i + 1)):
+        st.write("Miner Address: " + str(blocks[i].miner_address))
 
+        all_transactions = blocks[i].transactions
+        
+        for j in range(len(all_transactions)):
+            nb_transaction = "Transaction " + str(j + 1)
+            new_title = f'<p style="font-family:sans-serif; color:deeppink; font-size: 20px;">{nb_transaction}</p>'
+            st.markdown(new_title, unsafe_allow_html=True)
 
-
-# def draw_block(x, y, width, height, color, text):
-#     st.markdown(
-#         f"""
-#         <svg width="{width}" height="{height}">
-#             <rect x="{x}" y="{y}" width="{width}" height="{height}" style="fill:{color};stroke-width:3;stroke:rgb(0,0,0)" />
-#             <text x="{x + 10}" y="{y + 20}" fill="black">{text}</text>
-#         </svg>
-#         """,
-#         unsafe_allow_html=True,
-#     )
-
-
-# #draw a series of ten rectangles which are all the same size and color from left to right
-# for i in range(10):
-#     #define the color as red
-#     color = "#F63366"
-#     #draw the blocks from left to right
-#     draw_block(i*0.01, 0, 30, 30, color, "Block " + str(i + 1))
+            st.write("Sender: " + str(all_transactions[j].sender))
+            st.write("Receiver: " + str(all_transactions[j].receiver))
+            st.write("Amount: " + str(all_transactions[j].amount))
+            st.write("Date: " + str(all_transactions[j].date))
 
 
 # #define the color as red
 # color = "#F63366"
-# #draw a block in the bottom left corner 
-# draw_block(0, 0, 100, 100, color, "Block 1")
-
-
-
-
-
-
 
 
 
